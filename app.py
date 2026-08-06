f2972f52e3b2dfbcf8687cdc68e391312bb93423
jorggmnz <jorgemnz2.cc@gmail.com>
2026-08-05 22:04:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,24 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Jorge Muñoz")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
-valor_final = st.number_input("Ingrese el valor final")
 
-lista_numeros = list(range(int(valor_inicial),int(valor_final)))
-st.write(lista_numeros)
 
-                    
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+
+if modulos == "Módulo Listas":
+  
+  st.write("Bienvenido al módulo Listas")
+  
+  valor_inicial = st.number_input("Ingrese el valor inicial")
+  valor_final = st.number_input("Ingrese el valor final")
+  
+  lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+  st.write(lista_numeros)
+  
+elif modulos == "Módulo Arreglos":
+  
+  st.write("Bienvenido al módulo de Arreglos")
+  
+else:
+  
+  st.write("Bienvenido al módulo de Funciones")
